21c3441fa114c81749133159a24e6f02a0acd208
hai03kt <lysonhai8@gmail.com>
2025-03-03 15:53:09 +0700

    Revert "add ui"
    This reverts commit 86a2e9b30570d77f552ba65135a7e94e9f8fde5c.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -103,7 +103,6 @@ dependencies {
     implementation(libs.androidx.material3.android)
     implementation(libs.androidx.tv.material)
     implementation(libs.androidx.compose.material)
-    implementation(libs.androidx.ui.graphics.android)
     debugImplementation(compose.uiTooling)
 }
 

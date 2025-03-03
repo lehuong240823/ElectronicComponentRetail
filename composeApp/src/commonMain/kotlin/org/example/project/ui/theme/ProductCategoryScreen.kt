311d6d489fc0d448c76e3c2e75a305c4d1c051ab
hai03kt <lysonhai8@gmail.com>
2025-03-03 15:50:46 +0700

    Revert "Merge pull request #7 from lehuong240823/Hai"
    This reverts commit 7635c144bb7414b9d225814761949ea6c0256b15, reversing
    changes made to 657202f92a837afc1bdecbd06d1303bcc56d29f3.

--- composeApp/src/commonMain/kotlin/org/example/project/ui/theme/ProductCategoryScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/theme/ProductCategoryScreen.kt
@@ -1,15 +1,19 @@
-package com.example.ui
+package org.example.project.ui.theme
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.content.MediaType.Companion.Text
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.material3.CardElevation
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.tv.material3.CardDefaults
+import androidx.wear.compose.material.CardDefaults
 
 @Composable
 fun ProductCategoryScreen(onCategoryClick: (String) -> Unit) {
@@ -20,20 +24,20 @@ fun ProductCategoryScreen(onCategoryClick: (String) -> Unit) {
 
         LazyColumn {
             items(categories) { category ->
-                CategoryItem(category, onCategoryClick)
+                categoryItem(category, onCategoryClick)
             }
         }
     }
 }
 
 @Composable
-fun CategoryItem(category: String, onClick: (String) -> Unit) {
+fun categoryItem(category: String, onClick: (String) -> Unit) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
             .clickable { onClick(category) },
-        elevation = CardElevation.cardElevation(defaultElevation = 4.dp)
+        elevation = extracted().cardElevation(defaultElevation = 4.dp)
     ) {
         Text(
             text = category,
@@ -41,4 +45,8 @@ fun CategoryItem(category: String, onClick: (String) -> Unit) {
             modifier = Modifier.padding(16.dp)
         )
     }
+}
+
+private fun extracted() {
+    CagrdDefaults
 }

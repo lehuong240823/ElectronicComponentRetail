fac0748abffd4e71f3c343eca3a45803bed6f787
lehuong240823 <132122725+lehuong240823@users.noreply.github.com>
2025-03-10 08:16:11 +0700

    Update Strings.kt

--- composeApp/src/commonMain/kotlin/org/example/project/core/Strings.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/core/Strings.kt
@@ -1,8 +1,8 @@
 package org.example.project.core
 
-val API_KEY = "AIzaSyCnS08qKxWgFEKhF0Le5u4lT_EheqdE1TM"
+
 val GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/auth?"
-val OAUTH_CLIENT_ID = "10594710747-8kdh7tfuuh4dh63g29jl6laa08pkb00e.apps.googleusercontent.com"
+
 val DESKTOP_REDIRECT_URL = ""
 val WEB_REDIRECT_URL = ""
 val ANDROID_REDIRECT_URL = ""
